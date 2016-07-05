8249b0edb95e8dfb938f2ee6e8c02745fcd5140e
Wanzhi Du <levin@the520.cn>
2016-07-06 00:31:07 +0800

    enhance UI expirerence

--- assets/scripts/popup.js
+++ assets/scripts/popup.js
@@ -1,28 +1,17 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-var script = '';
-
 document.addEventListener('DOMContentLoaded', function () {
+    var clipboard = new Clipboard('.btn');
+    clipboard.on('success', function(e) {
+        var btn = document.querySelector('button');
+        btn.setAttribute('aria-label', '复制成功');
+        setTimeout(function(){
+            btn.setAttribute('aria-label', '点击复制');
+        }, 2000)
+    });
+
     chrome.tabs.executeScript(null,
     {
         code: 'document.body.innerHTML.match(\/"spuid"\s?:\\s?"(.*?)"\\s?\/)[1]'
     }, function(data){
         document.querySelector('input#spuid').value = data;
     })
-
-    // chrome.tabs.executeScript(null,
-    // {
-    //     code: 'document.body.innerHTML.match(\/"spuid"\s?:\\s?"(.*?)"\\s?\/)[1]'
-    // }, function(data){
-    //     document.querySelector('input#spuid').value = data;
-    // })
-
-    // chrome.tabs.executeScript(null,
-    // {
-    //     code: 'document.body.innerHTML.match(\/"spuid"\s?:\\s?"(.*?)"\\s?\/)[1]'
-    // }, function(data){
-    //     document.querySelector('input#spuid').value = data;
-    // })
 });
