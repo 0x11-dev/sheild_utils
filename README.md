c33661d393147ad0af60e4d4cbd5bf5d2b33e1e0
Wanzhi Du <levin@the520.cn>
2016-07-18 11:41:55 +0800

    新增查询商详页api功能

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 - Extract SPUID
 - Extract URINO
 - Extract SOURCE SITE URL
+- 查询商品库API
+- 查询商详接口API
 
 ### Author
 contact levin for support.
--- assets/scripts/popup.js
+++ assets/scripts/popup.js
@@ -14,11 +14,17 @@ document.addEventListener('DOMContentLoaded', function () {
         window.open('http://121.40.253.245:8897/prodb?type=info&url=' + encodeURIComponent(value));
     });
 
+    document.querySelector('#btnQueryBiz').addEventListener('click', function(){
+        var value = document.querySelector('#queryspuid').value;
+        window.open('http://120.26.117.54:8890/server3/detailPage4SpuBySpuid?spuid=' + encodeURIComponent(value));
+    });
+
     chrome.tabs.executeScript(null,
     {
         code: 'document.body.innerHTML.match(\/"spuid"\s?:\\s?"(.*?)"\\s?\/)[1]'
     }, function(data){
         document.querySelector('input#spuid').value = data;
+        document.querySelector('input#queryspuid').value = data;
     })
 
     chrome.tabs.executeScript(null,
